bdaf17250a2b6219ae79f5985cd46e89b018ca91
worldsham <shamoshamilov@yandex.ru>
2023-11-09 00:51:00 +0300

    Домашнее задание №2

---
+++ test_homework.py
@@ -0,0 +1,23 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+
+
+@pytest.fixture
+def browser_configs():
+    browser.config.window_width = 1690
+    browser.config.window_height = 1080
+    yield
+    browser.quit()
+
+
+def test_google_search():
+    browser.open('https://www.google.ru/')
+    browser.element('#APjFqb').should(be.blank).type('yashaka/selen').press_enter()
+    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_google_search_not_found():
+    browser.open('https://www.google.ru/')
+    browser.element('#APjFqb').should(be.blank).type('fefefeffefe').press_enter()
+    browser.element('#appbar').should(have.text('Результатов fefefeffefe не найдено '))
